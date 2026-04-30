c547ad95635208a4867ce7e1b5113b3e839bc827
zong <69517696+caizongxun@users.noreply.github.com>
2026-04-30 14:25:59 +0800

    Show Hello World on launch

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -1,37 +1,18 @@
 import SwiftUI
-import RealityKit
 
 struct ContentView: View {
 
-    @State private var showImmersiveSpace = false
-    @Environment(\.openImmersiveSpace) var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
-
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Apple Vision Pro App")
+        VStack(spacing: 16) {
+            Text("Hello World")
                 .font(.extraLargeTitle)
-                .padding()
 
-            Text("Welcome to visionOS")
-                .font(.title)
+            Text("Welcome to Apple Vision Pro")
+                .font(.title3)
                 .foregroundStyle(.secondary)
-
-            Toggle("Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding()
         }
         .padding(40)
         .glassBackgroundEffect()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    await openImmersiveSpace(id: "ImmersiveSpace")
-                } else {
-                    await dismissImmersiveSpace()
-                }
-            }
-        }
     }
 }
 
